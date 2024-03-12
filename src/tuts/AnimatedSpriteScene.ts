cfebed1dd11d34e3426fcfa291317c6e1fd180e3
KrupalHungryBear <kj@hungrybeargaming.co.uk>
2024-03-12 10:28:39 +0000

    Added images

---
+++ src/tuts/AnimatedSpriteScene.ts
@@ -0,0 +1,29 @@
+import { AnimatedSprite, Container, Texture } from "pixi.js";
+
+export class Scene extends Container {
+    constructor() {
+        super();
+
+        // This is an array of strings, we need an array of Texture
+        const clampyFrames: Array<String> = [
+          "banana.png.png",
+          "kitty.png",
+          "clampy.png",
+        ];
+
+        // `array.map()` creates an array from another array by doing something to each element.
+        // `(stringy) => Texture.from(stringy)` means
+        // "A function that takes a string and returns a Texture.from(that String)"
+        const animatedClampy: AnimatedSprite = new AnimatedSprite(clampyFrames.map((stringy) => Texture.from(stringy)));
+        // (if this javascript is too much, you can do a simple for loop and create a new array with Texture.from())
+
+        this.addChild(animatedClampy); // we just add it to the scene
+
+        // Now... what did we learn about assigning functions...
+        animatedClampy.onFrameChange = this.onClampyFrameChange.bind(this);
+    }
+
+    private onClampyFrameChange(currentFrame: any): void {
+        console.log("Clampy's current frame is", currentFrame);
+    }
+}
