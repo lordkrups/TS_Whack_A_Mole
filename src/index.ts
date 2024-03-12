a287a3022e11d9d6c4f7fa350410105079d553ee
KrupalHungryBear <kj@hungrybeargaming.co.uk>
2024-03-12 16:27:32 +0000

    Added ticker scene

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { Application } from 'pixi.js'
-import { Scene } from './tuts/AnimatedSpriteScene'; // This is the import statement
+// import { Scene } from './scenes/Scene'; // This is the import statement
+// import { Scene } from './tuts/AnimatedSpriteScene'; // This is the import statement
+import { Scene } from './tuts/TickerScene'; // This is the import statement
 
 const app = new Application<HTMLCanvasElement>({
     view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
@@ -10,7 +12,8 @@ const app = new Application<HTMLCanvasElement>({
     height: 480
 });
 
+const sceny: Scene = new Scene(app.screen.width, app.screen.height);
 // pass in the screen size to avoid "asking up"
-const sceny: Scene = new Scene();
+// const sceny: Scene = new Scene();
 
 app.stage.addChild(sceny)
